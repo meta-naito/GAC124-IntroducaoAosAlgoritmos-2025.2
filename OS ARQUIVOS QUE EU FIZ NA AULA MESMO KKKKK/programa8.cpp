ac53b8a4d5529be609481c1cf777adf5db33cdb7
Lívia Masson <liviarezende04812@gmail.com>
2025-09-02 13:28:27 -0300

    Arquivos de aula
    exercícios 1- ao 9 que fiz durante a aula.
    não estão otimizados, mas coloquei para mostrar a "evolução" do meus códigos em algumas semanas :p

---
+++ OS ARQUIVOS QUE EU FIZ NA AULA MESMO KKKKK/programa8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	float nota1, nota2, media;
+	cin >> nota1;
+	cin >> nota2;
+	media = ((nota1*4) +(nota2*3))/7;
+	cout << media;
+
+}
+
+
