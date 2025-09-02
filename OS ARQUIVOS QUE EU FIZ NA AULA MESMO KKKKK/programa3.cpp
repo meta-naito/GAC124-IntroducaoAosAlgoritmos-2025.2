38fae874825515e5b4bdf0b7927e01585abc3b33
Lívia Masson <liviarezende04812@gmail.com>
2025-09-02 13:33:58 -0300

    Delete OS ARQUIVOS QUE EU FIZ NA AULA MESMO KKKKK directory
    meu DEEEUS

--- OS ARQUIVOS QUE EU FIZ NA AULA MESMO KKKKK/programa3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main() {
-	int numero; //int nesse caso e pra indicar que "numero" é pra ser INTEIRO TA BOM??????????
-	cin >> numero;
-	cout << numero/3;
-	return 0;
-}
