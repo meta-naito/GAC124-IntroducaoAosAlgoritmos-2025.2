d3e2f262dcdaa8d43a33eaaa508cbab8a7db6a3f
via <liviarezende04812@gmail.com>
2025-09-26 17:44:54 -0300

    problema 15 incompleto

--- semana 6/problema15/problema15.cpp
+++ semana 6/problema15/problema15.cpp
@@ -2,3 +2,31 @@
 #include <iostream>
 using namespace std;
 
+
+void mdc(int& numerador, int& denominador) {
+    //achar o MDC
+    int aux = 2;
+    bool divisor = true;
+
+    while ((aux <= numerador) or (aux <= denominador)) {
+        if ((numerador % aux == 0) and (denominador % aux == 0)) {
+            divisor = true;
+            
+            if (divisor) {
+                numerador /= divisor;
+                denominador /= divisor;
+            }
+        }
+
+        aux++;
+    }
+
+}
+
+int main () {
+    int numerador, denominador;
+
+
+
+    return 0;
+}
