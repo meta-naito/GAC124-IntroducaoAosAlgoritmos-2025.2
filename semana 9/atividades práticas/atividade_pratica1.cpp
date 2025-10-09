7f08c90f478844ae9b846cb6b622264cbd98b12c
via <liviarezende04812@gmail.com>
2025-10-09 20:16:44 -0300

    criação pastas semana 8 e 9, atividades práticas incompletas

---
+++ semana 9/atividades práticas/atividade_pratica1.cpp
@@ -0,0 +1,26 @@
+//atividade prática 1, semana 9
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+typedef string str; //muita preguiça de ficar escrevendo string; valeu python!
+
+str
+
+
+int main() {
+    str nomeArq, linha = "";
+
+    cin >> nomeArq;
+
+    ifstream entrada(nomeArq);
+        getline(entrada, linha);
+
+    
+    
+    
+
+
+    return 0;
+}
---
+++ semana 9/atividades práticas/atividade_pratica2.cpp
@@ -0,0 +1,24 @@
+//atividade prática 2, semana 9
+#include <iostream>
+#include <string>
+
+typedef string str;
+using namespace std;
+
+void lerListas (int& n) {
+
+    cin >> n;
+    str lista[n];
+    
+}
+
+
+
+int main() {
+    int n = 0;
+    
+    lerListas(n);
+
+
+    return 0;
+}
