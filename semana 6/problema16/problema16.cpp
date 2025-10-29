a3b5cf860359f5134a8716fff810a4a0989c0b0b
via <liviarezende04812@gmail.com>
2025-10-29 03:01:25 -0300

    ex 16

--- semana 6/problema16/problema16.cpp
+++ semana 6/problema16/problema16.cpp
@@ -3,25 +3,24 @@
 #include <cmath>
 using namespace std;
 
-double razaoAurea (int);
+double razaoAurea(int razao, double& phi) {
+    double valor = 1.0;
+
+    for (int i = 0; i < razao; i++) {
+        valor = sqrt(1 + valor);
+    }
+
+    phi = valor;
+
+    return phi;
+}
 
 int main() {
     int termoRaiz = 0;
+    double phi = 0.0;
 
     cin >> termoRaiz;
-
-    cout << razaoAurea(termoRaiz) << endl;
+    cout << razaoAurea(termoRaiz, phi) << endl;
 
     return 0;
-}
-
-double razaoAurea(int r) {
-    double phi = 0;
-
-    for (int i = 0; i >= r; i++) {
-        phi += sqrt(1);
-    }
-
-
-    return phi;
 }
