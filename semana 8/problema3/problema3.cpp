f74f06be06292391688a4cf309457b42bd195e3c
via <liviarezende04812@gmail.com>
2025-11-18 12:35:05 -0300

    exs 3 e (incompleto) 4

--- semana 8/problema3/problema3.cpp
+++ semana 8/problema3/problema3.cpp
@@ -1,27 +1,48 @@
 //semana 8, ex 3
 #include <iostream>
-using namespace std;
 
 void leituraVetor(int *&vetor, int tam) {
     for (int i = 0; i < tam; i++) {
-        cin >> vetor[i];
+        std::cin >> vetor[i];
     }
 
     return;
 }
 
+bool calculaMultiplo(int *vetor, int indice, int multiplo) {
+    return (vetor[indice] % multiplo == 0);
+}
+
+void imprimeMultiplos(int *vetor, int tam, int multiplo) {
+    int cont = 0; 
+    
+    for (int i = 0; i < tam; i++) {
+        if (calculaMultiplo(vetor, i, multiplo)) { //se for true, nn é recomendado especificar nesse caso que é true
+            std::cout << vetor[i] << " ";
+            cont++;
+        }
+    }
+
+    if (cont == 0) { //se nao tiver nenhum multiplo, ja que cont nn for incrementado
+        std::cout << 0 << std::endl;
+    }
+    else {
+        std::cout << std::endl;
+    }
+    
+    return;
+}
+
 int main() {
     int qNum = 0;
-    cin >> qNum;
+    std::cin >> qNum;
 
     int *vetor = new int[qNum];
     leituraVetor(vetor, qNum);
 
-    for (int i = 0; i < qNum; i++) {
-        if (vetor[i] % 2 == 0) {
-            cout << vetor[i] << " ";
-        }
-    }
+    imprimeMultiplos(vetor, qNum, 2);
+    imprimeMultiplos(vetor, qNum, 3);
+    imprimeMultiplos(vetor, qNum, 6);
 
     delete [] vetor;
 
---
+++ semana 8/problema4/problema4.cpp
@@ -0,0 +1,12 @@
+//semana 8, ex 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    int qNum;
+    cin >> qNum;
+
+    
+
+    return 0;
+}
